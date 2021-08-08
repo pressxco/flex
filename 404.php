574e0ac4b8b3a20bb0deff173ffb13f346e2ce2d
Bilal TAS <bilaltas@me.com>
2021-08-08 17:11:01 +0300

    HTML aligned to the left

--- 404.php
+++ 404.php
@@ -14,27 +14,27 @@ fx_layout(
 	function() {
 		?>
 
-	<div id="page-404" class="container items-center px-8 mx-auto text-center">
+<div id="page-404" class="container items-center px-8 mx-auto text-center">
 
-		<div>
-			<h1 class="text-404"><?php esc_html_e( '404', 'flex' ); ?></h1>
+	<div>
+		<h1 class="text-404"><?php esc_html_e( '404', 'flex' ); ?></h1>
 
-				<p class="text-gray-600"><?php esc_html_e( 'Unfortunately, we could not find the page you are looking for.', 'flex' ); ?></p>
+			<p class="text-gray-600"><?php esc_html_e( 'Unfortunately, we could not find the page you are looking for.', 'flex' ); ?></p>
 
-				<p>
+			<p>
 
-					<a class="inline-flex items-center text-blue-600" href="<?php esc_attr( home_url() ); ?>">
+				<a class="inline-flex items-center text-blue-600" href="<?php esc_attr( home_url() ); ?>">
 
-						<?php fx_icon( 'arrow-left' ); ?>
+					<?php fx_icon( 'arrow-left' ); ?>
 
-						<span class="ml-1"><?php esc_html_e( 'Get back to the home page', 'flex' ); ?></span>
+					<span class="ml-1"><?php esc_html_e( 'Get back to the home page', 'flex' ); ?></span>
 
-					</a>
+				</a>
 
-				</p>
-		</div>
-
+			</p>
 	</div>
+
+</div>
 
 		<?php
 	}
