ab9abd25f8f9ea6e01b96183fc993b510b718366
Atakan Öz <atakanozofficial@gmail.com>
2021-08-14 14:29:58 +0300

    block styles fixed

--- functions.php
+++ functions.php
@@ -321,3 +321,11 @@ require get_template_directory() . '/includes/fallbacks/menu-fallback.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/includes/utilities/jetpack.php';
 }
+
+// Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css() {
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
