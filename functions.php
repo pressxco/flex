c810d1c5119d915be04310640fd054fcda33f07f
Atakan Öz <atakanozofficial@gmail.com>
2021-08-14 15:08:40 +0300

    minor fix

--- functions.php
+++ functions.php
@@ -330,4 +330,4 @@ function fx_remove_block_library() {
 	wp_dequeue_style( 'wp-block-library-theme' );
 	wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
 }
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+add_action( 'wp_enqueue_scripts', 'fx_remove_block_library', 100 );
