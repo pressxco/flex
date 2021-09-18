8fc583d8ba76bad8ed4b3f5ce5c56b4d6ab7174c
Bilal TAS <bilaltas@me.com>
2021-09-19 02:45:00 +0300

    Use sidebar layout

--- archive.php
+++ archive.php
@@ -13,38 +13,20 @@
 
 <?php
 fx_layout(
-	'default',
+	'sidebar',
 	function () {
 		?>
 
 		<div class="archive-title">
 
-		<?php
-			the_archive_title( '<h1 class="page-title">', '</h1>' );
-			the_archive_description( '<div class="taxonomy-description">', '</div>' );
-		?>
+			<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			?>
 
 		</div>
 
-		<div class="grid grid-cols-3 gap-7">
-
-			<div class="col-span-3 md:col-span-2">
-
-				<div class="post-loop space-y-7">
-
-					<?php fx_render( 'views/blog/post-loop' ); ?>
-
-				</div>
-
-			</div>
-
-			<div class="col-span-3 md:col-span-1">
-
-				<?php get_sidebar(); ?>
-
-			</div>
-
-		</div>
+		<?php fx_render( 'views/blog/post-loop' ); ?>
 
 		<?php
 	}
