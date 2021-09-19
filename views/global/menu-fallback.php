ccf4469bf61f708741c49e8d602303e5c44fa091
Bilal TAS <bilaltas@me.com>
2021-09-19 13:58:19 +0300

    HTML fixes and optimizations

--- views/global/menu-fallback.php
+++ views/global/menu-fallback.php
@@ -10,11 +10,10 @@
 ?>
 <nav class="primary">
 	<ul class="primary-menu">
-		<li id="menu-item-1303" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1303">
+		<li class="menu-item">
 			<a href="<?php echo esc_url( admin_url( 'nav-menus.php' ) ); ?>">
-				<?php esc_html_e( 'Set your primary menu here', 'flex' ); ?>
-				<span class="chevron"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" display="block" id="ArrowRight"><path d="M4 12h16"/><path d="M13 5l7 7-7 7"/></svg></svg>
-			</span>
-		</a>
+				<?php esc_html_e( 'Set your primary menu here →', 'flex' ); ?>
+			</a>
+		</li>
 	</ul>
 </nav>
