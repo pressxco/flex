25560d3e9fbca435ea56a2cb3d0918c6c1e2c1b7
Bilal TAS <bilaltas@me.com>
2021-10-16 16:05:29 +0300

    Optional configurations and dependencies removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   mode: 'jit',
-  purge: {
-    content: [
+  purge: [
       './sources/**/*.{svg,css,png,jpg,js}',
       './includes/**/*.php',
       './views/**/*.php',
       './safelist.txt',
 
-      // // Theme root PHP files
+      // Theme root PHP files
       './404.php',
       './archive.php',
       './comments.php',
@@ -21,66 +18,5 @@ module.exports = {
       './search.php',
       './sidebar.php',
       './single.php',
-    ],
-    options: {
-      safelist: [],
-      blocklist: [],
-      keyframes: true,
-      fontFace: true,
-    },
-  },
-  darkMode: 'class',
-  theme: {
-    extend: {
-      spacing: {
-        colors: {
-          transparent: 'transparent',
-          current: 'currentColor',
-          black: '#000',
-          white: '#fff',
-          amber: colors.amber,
-          blue: colors.blue,
-          cyan: colors.cyan,
-          emerald: colors.emerald,
-          fuchsia: colors.fuchsia,
-          gray: colors.coolGray,
-          green: colors.green,
-          indigo: colors.indigo,
-          lime: colors.lime,
-          orange: colors.orange,
-          pink: colors.pink,
-          purple: colors.purple,
-          red: colors.red,
-          rose: colors.rose,
-          teal: colors.teal,
-          violet: colors.violet,
-          yellow: colors.yellow,
-        },
-      },
-    },
-    plugins: [
-      require('@tailwindcss/line-clamp'),
-      require('@tailwindcss/typography'),
-      require('@tailwindcss/forms'),
-      function ({ addComponents }) {
-        addComponents({
-          '.container': {
-            maxWidth: '1170px',
-            '@screen sm': {
-              maxWidth: '100%',
-            },
-            '@screen md': {
-              maxWidth: '100%',
-            },
-            '@screen lg': {
-              maxWidth: '1140px',
-            },
-            '@screen xl': {
-              maxWidth: '1170px',
-            },
-          }
-        })
-      }
-    ]
-  }
+  ]
 }
